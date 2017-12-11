0c5754f7162213547e1aa467f1a8fc38455f6e89
Unknown <christopher.benevent@gmail.com>
2017-12-11 11:30:03 +0100

    Auto stash before merge of "push_test" and "origin/push_test"

--- index.php
+++ index.php
@@ -2,11 +2,28 @@
 
   $title = "Movies!";
 
+  //tableau pour les images
   $colors = ["jaune", "vert", "bleu"];
   
       foreach($colors as $color){
           echo $color . '</br>';
       }
+
+
+
+      $book = [
+            "title" => "blablabla",
+            "year" => 2006,
+            "author" => "Johnny"
+        ];
+
+        $book2 = [
+            "title" => "yeah",
+            "year" => 2015,
+            "author" => "Alphonse"
+        ];
+
+        $books = [$book, $book2];
 
 ?>
 
@@ -63,6 +80,8 @@
 
     <h1><?php echo $title; ?></h1>
 
+    <h2><?php echo $book["title"]; ?></h2>
+    <h2><?php echo $book2["title"]; ?></h2>
 
 
 
