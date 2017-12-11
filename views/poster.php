25c28a18ff0511a18c973664d1ff3ba666926438
Unknown <benjamincaillet14@gmail.com>
2017-12-11 14:35:51 +0100

    Ajout de la fonction random aléatoire dans les 50

--- views/poster.php
+++ views/poster.php
@@ -10,7 +10,7 @@
 //Association de la base de données
 include ("db.php");
 
-$sql = "SELECT imdbId FROM movie_simple LIMIT 50";
+$sql = "SELECT imdbId FROM movie_simple ORDER BY RAND() LIMIT 50";
 $stmt = $dbh -> query($sql); //execution de la requête
 $movies = $stmt -> fetchAll(); //récuperer toutes les lignes de la requête.
 //var_dump($movies); //afficher les lignes
