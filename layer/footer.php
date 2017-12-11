5dc19a9d556163eeee79b5053a94c64147cbb4cd
Unknown <benjamincaillet14@gmail.com>
2017-12-11 15:37:29 +0100

    Footer

--- layer/footer.php
+++ layer/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="foot">
-        <h4 class="card-title">Réalisé par Christopher Bénévent et son fidèle écuyer Ben</h4>
-        <p class="card-text">Allo Cinoche - All Rights Reserved fuckers</p>
+        <h4 class="card-title">Fièrement réalisé par l'équipe Allo Cinoche</h4>
+        <p class="card-text">Allo Cinoche - All Rights Reserved 2017</p>
     </div>
 </footer>
