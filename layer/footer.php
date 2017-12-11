221a2bf6c3fc5886903c9836fc23277b24a14a8c
Unknown <christopher.benevent@gmail.com>
2017-12-11 14:01:07 +0100

    layer footer

---
+++ layer/footer.php
@@ -0,0 +1,7 @@
+<footer>
+<div class="card bg-dark text-white">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text">Last updated 3 mins ago</p>
+</div>
+</footer>
