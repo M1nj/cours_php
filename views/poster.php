861dcad04b0c498c4f70d31456ace2067c32c6e9
Unknown <benjamincaillet14@gmail.com>
2017-12-11 13:46:26 +0100

    Affichage des posters en allant interroger la BDD

--- views/poster.php
+++ views/poster.php
@@ -4,12 +4,22 @@
 
 
 <div class="posters">
+
 <?php
-$images= glob('img/posters/*.jpg', GLOB_BRACE);
-foreach ($images as $image){
+
+//Association de la base de données
+include ("db.php");
+
+$sql = "SELECT imdbId FROM movie_simple LIMIT 49";
+$stmt = $dbh -> query($sql); //execution de la requête
+$movies = $stmt -> fetchAll(); //récuperer toutes les lignes de la requête.
+//var_dump($movies); //afficher les lignes
+
+//$images= glob('img/posters/*.jpg', GLOB_BRACE);
+foreach ($movies as $movie){
     echo '
         <li>
-            <img src="' .$image. '" class="img-poster">
+            <img src="img/posters/' .$movie["imdbId"].'.jpg" class="img-poster">
         </li>';
     }
 ?>
