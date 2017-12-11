b80ac7e3aa049b807c9674f2dceac50b157d9e7f
Unknown <christopher.benevent@gmail.com>
2017-12-11 15:36:35 +0100

    Ben

--- layer/footer.php
+++ layer/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="foot">
-        <h4 class="card-title">Réalisé par Christopher Bénévent</h4>
+        <h4 class="card-title">Réalisé par Christopher Bénévent et son fidèle écuyer Ben</h4>
         <p class="card-text">Allo Cinoche - All Rights Reserved fuckers</p>
     </div>
 </footer>
