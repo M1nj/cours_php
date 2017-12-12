b52b416ea832fc5fc40c8c71af2adcc0b5dff2c6
Unknown <benjamincaillet14@gmail.com>
2017-12-12 11:42:12 +0100

    Auto stash before merge of "master" and "origin/push_test"

---
+++ views/resa.php
@@ -0,0 +1,86 @@
+<?php 
+include("../db.php");
+
+//traiter le form
+        //récupérer les données
+       // print_r($_POST);
+
+        $name = $_POST["username"];
+        $title = $_POST["title"];
+        $critic = $_POST["critic"];
+
+        $error = "";
+
+        //valide les données
+            //nom renseigné ?
+        if (empty($name)){
+            $error = "veuillez renseigner votre username";
+        }
+
+        if (empty($title)){
+            $error = "veuillez renseigner le titre de votre critique";
+        }
+
+        if (empty($critic)){
+            $error = "veuillez renseigner votre critique";
+        }
+            
+         /*   //date dans le futur ? 
+        if ($date < date("Y-m-d")){
+            $error = "blabla.";
+        }  */
+        
+            //téléphone ?
+            //nb de personne ?
+
+        //si les données sont valides
+        if ($error == ""){
+            //ajout dans la bdd
+            /*
+            INSERT INTO booking 
+            VALUES 
+            (NULL, 'guillaume', '2018-01-01', 12, '0634444444', 'près de la fenêtre svp', NOW())
+            */
+            $sql = "INSERT INTO review 
+                    VALUES (NULL, :title, :username, 
+                    :critic, NOW())";
+
+            $stmt = $dbh->prepare($sql);
+            $stmt -> execute([
+                ":title" => $title,
+                ":username" => $name, 
+                ":critic" =>$critic,
+    
+            ]);
+
+            //afficher un message de succès
+            //redirige
+            header("Location: https://lingscars.com");
+        }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Réservation</title>
+</head>
+<body>
+    <!-- afficher le formulaire --> 
+    <form method="post">
+
+        <label>Votre username</label>
+        <input type="username" name="Pseudo">
+
+        <label>Titre de votre critique</label>
+        <input type="text" name="title">
+
+        <label>Votre critique</label>
+        <input type="text" name="critique">
+
+        <div><?php echo $error; ?></div>
+        <button>Envoyer ma réservation</button>
+    </form>
+</body>
+</html>
