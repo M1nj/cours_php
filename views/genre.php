9e142d8db5a09c9244ea5a964a314ce591a2859b
Unknown <benjamincaillet14@gmail.com>
2017-12-13 11:38:59 +0100

    Boutons sur la page d'accueil et sur les pages de genres, css sur les pages de genre, affichage des commentaires

--- views/genre.php
+++ views/genre.php
@@ -5,10 +5,11 @@
     $genre = $_GET['genre']; //on récupère l'ID dans l'URL
     
     $sql = "SELECT * FROM movie_simple
-            WHERE genres LIKE :genre"; //utiliser les deux points lorsque les données peuvent être manipulées par l'utilisateur.
+            WHERE genres LIKE :genre 
+            LIMIT 50"; //utiliser les deux points lorsque les données peuvent être manipulées par l'utilisateur.
     
     $stmt = $dbh -> prepare($sql);
-    $stmt -> execute([":genre" => "%".$genre."%",]); //on la remplace ensuite dans $id.
+    $stmt -> execute([":genre" => "%".$genre."%",]); //on la remplace ensuite dans $genre.
     $movies = $stmt -> fetchAll();
     
     $title='Détails du film'; 
@@ -30,6 +31,25 @@
 <body>
 <?php 
 
+$sql = "SELECT * FROM genre ORDER BY name";
+$stmt = $dbh -> query($sql); //execution de la requête
+$genres = $stmt -> fetchAll(); //récuperer toutes les lignes de la requête.
+//var_dump($movies); //afficher les lignes
+
+    echo '<div class=liste_genre>';
+foreach ($genres as $list_genre){
+    //echo '<a class="btn btn-primary drama" href="../index.php" role="button">';
+    echo '<a class="btn btn-primary '.strtolower($list_genre["name"]).'" href="../views/genre.php?genre='.strtolower($list_genre["name"]).'" role="button">';
+    echo $list_genre["name"];
+    echo '</a>';
+    }
+    echo '</div>';
+
+
+echo '<h1>'.strtoupper($genre).'</h1>';
+?>
+<ul class="posters">
+<?php
 foreach ($movies as $movie){
     echo '
         <li>
@@ -44,6 +64,7 @@ foreach ($movies as $movie){
     }
 
 ?>
+</ul>
 </body>
 
 <?php include("..layer/footer.php"); ?>
