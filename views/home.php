cd1167dc3f6ae597987e7b01414551538a13b176
Unknown <benjamincaillet14@gmail.com>
2017-12-14 17:41:43 +0100

    Ajout image accueil + css menu

--- views/home.php
+++ views/home.php
@@ -1,6 +1,13 @@
 <?php
 $title='Accueil';
 ?>
+
+<div class="accueil">
+    <img src="img/cinema.png" alt="image d'accueil">
+
+
+</div>
+
 <h1><?php echo $title; ?></h1>
 <section>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis massa lorem, aliquam ut ultricies et, feugiat id urna. Donec sed tempor enim. Etiam suscipit iaculis porttitor. Nullam tempor diam quis dapibus vehicula. Curabitur in tincidunt elit. Nunc accumsan pulvinar lacus. Duis tristique commodo commodo. Donec eu magna lobortis, tincidunt nisi eu, porta eros. Proin quis lorem ipsum. Proin cursus arcu at aliquam posuere. Sed eu scelerisque diam. Ut hendrerit egestas massa non auctor. Praesent ut lobortis quam.</p>
